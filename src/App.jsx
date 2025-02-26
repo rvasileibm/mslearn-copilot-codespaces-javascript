53ae4fc9b34348127dad9769822f7257b27e2624
rvasileibm <razvan.vasile1@ro.ibm.com>
2025-02-26 20:17:40 +0000

    Copilot commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Razvan Vasile",
+  title: "DevOps Engineer",
+  email: "rvasile@example.com",
+  gitHub: "ibm",
+  instagram: "ibm",
+  linkedIn: "ibm",
   medium: "",
-  twitter: "microsoft",
+  twitter: "ibm",
   youTube: "Code",
 };
 
